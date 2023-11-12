dffa70364284e06cad965edc29b1ae9bd93c4988
kalanithi96 <kalanithi96@gmail.com>
2023-11-12 22:26:13 +0530

    Added minor styles and Image tag in Meme component

--- 6.MemeGenerator/src/components/Meme.tsx
+++ 6.MemeGenerator/src/components/Meme.tsx
@@ -1,7 +1,12 @@
+import url from "./../assets/doge.png"
+
 export default function Meme(){
+    function getMemeImage(){
+        console.log("Hello")
+    }
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top Text"
@@ -12,8 +17,14 @@ export default function Meme(){
                     placeholder="Bottom Text"
                     className="form--input"
                 />
-                <button className="form--button">Get a new meme image 📸</button>
-            </form>
+                <button 
+                    className="form--button" 
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 📸
+                </button>
+            </div>
+            <img src={url} className="meme--image"/>
         </main>
     )
 }
