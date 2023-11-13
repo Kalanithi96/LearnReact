2346524b4c78c5703bb8365a66c5dffa4a164fbb
kalanithi96 <kalanithi96@gmail.com>
2023-11-13 22:42:10 +0530

    Added use effect to make API call and print response

--- 11.useEffect/src/App.tsx
+++ 11.useEffect/src/App.tsx
@@ -1,11 +1,18 @@
 import {useState, useEffect} from "react";
 
 function App() {
+  const [starWarsData, setStarWarsData] = useState({});
+
+  useEffect(()=>{
+    fetch("https://swapi.dev/api/people/1")
+      .then(res=>res.json())
+      .then(data=>setStarWarsData(JSON.stringify(data)))
+  }, [starWarsData])
 
   return (
     <>
-      <h1>Use Effect App</h1>
-    </>
+      <p>{JSON.stringify(starWarsData,null,2)}</p>
+    </>    
   )
 }
 
